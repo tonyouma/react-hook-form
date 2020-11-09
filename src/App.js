54ed9ea2f6a2ca8545e62855f8338011ccd0995d
TonyOuma <kyrietony@gmail.com>
2020-11-10 00:42:01 +0300

    fix(app.js): remove extra checks for validation

--- src/App.js
+++ src/App.js
@@ -13,19 +13,27 @@ function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control">
           <label>Email</label>
-          <input type="text" name="email" ref={register({required : true, pattern: /^[^@]+@[^@]+\.[^@.]{2,}$/})} />
-          {errors.email && errors.email.type === "required" && (
-            <p className="errorMsg">Email is required</p>
-          )}
-          {errors.email  && errors.email.type === "pattern" && (
-            <p className="errorMsg">Email is not valid</p>
-          )}
+          <input type="text" name="email" ref={
+          register({required : 'Email is required', 
+          pattern: {
+            value: /^[^@]+@[^@]+\.[^@.]{2,}$/, 
+          message: 'Email is not valid'
+          }
+          })} />
+          {errors.email &&
+            <p className="errorMsg">{errors.email.message}</p>
+          }
         </div>
         <div className="form-control">
           <label>Password</label>
-          <input type="password" name="password" ref={register({required: true, minLength: 6})}/>
-          {errors.password && errors.password.type === "minLength" && (
-            <p className="errorMsg">Password should be at-least 6 characters</p>
+          <input type="password" name="password" ref={
+            register({required: 'Password is required.', minLength: {
+              value: 6,
+              message: 'Password should be at-least 6 characters.'
+            } 
+            })}/>
+          {errors.password && (
+            <p className="errorMsg">{errors.password.message}</p>
           ) }
         </div>
         <div className="form-control">
