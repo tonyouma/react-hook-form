18d70201f27860bee0ef38b7d129476d81831499
TonyOuma <kyrietony@gmail.com>
2020-11-10 00:06:23 +0300

    feat(app.js): add validations to the form

--- src/App.js
+++ src/App.js
@@ -13,11 +13,20 @@ function App() {
       <form onSubmit={handleSubmit(onSubmit)}>
         <div className="form-control">
           <label>Email</label>
-          <input type="text" name="email" ref={register} /> 
+          <input type="text" name="email" ref={register({required : true, pattern: /^[^@]+@[^@]+\.[^@.]{2,}$/})} />
+          {errors.email && errors.email.type === "required" && (
+            <p className="errorMsg">Email is required</p>
+          )}
+          {errors.email  && errors.email.type === "pattern" && (
+            <p className="errorMsg">Email is not valid</p>
+          )}
         </div>
         <div className="form-control">
           <label>Password</label>
-          <input type="password" name="password" ref={register}/>
+          <input type="password" name="password" ref={register({required: true, minLength: 6})}/>
+          {errors.password && errors.password.type === "minLength" && (
+            <p className="errorMsg">Password should be at-least 6 characters</p>
+          ) }
         </div>
         <div className="form-control">
           <label></label>
